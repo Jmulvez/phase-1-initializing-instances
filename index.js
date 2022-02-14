9837f91ab966c7cb0206391fd87f96482137e9ba
John <johnmulvey48@yahoo.com>
2022-02-14 11:08:36 -0500

    git@github.com:Jmulvez/phase-1-initializing-instances.git

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Write your code here
+class breakfast {
+    constructor(food, drink) {
+        this.food = food;
+        this.drink = drinnk;
+    }
+}
+
+class lunch {
+    constructor(salad, soup, drink) {
+        this.salad = salad;
+        this.soup = soup;
+        this.drink = drink;
+    }
+}
+
+class dinner {
+    #dessert;
+    constructor(salad, soup, entree, drink, dessert) {
+        this.salad = salad;
+        this.soup = soup;
+        this.entree = entree;
+        this.drink = drink;
+        this.dessert = dessert;
+    }
+}
